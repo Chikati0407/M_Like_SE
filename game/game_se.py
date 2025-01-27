116f24eb7855539910284bf5ad6b72182cf3fe9e
Chikati0407 <130910609+Chikati0407@users.noreply.github.com>
2025-01-27 17:41:07 +0900

    プラットフォームに応じたマウス設定の追加と音声トリガーの改善

--- game/game_se.py
+++ game/game_se.py
@@ -1,15 +1,38 @@
 import pyxel
+import platform
+import PyxelUniversalFont as puf
+
+try:
+    from js import navigator
+    is_web_launcher = True
+except ImportError:
+    is_web_launcher = False
 
 class M_like_SE():
     def __init__(self):
 
+        if is_web_launcher:
+            # Web launcherから起動している場合、js関数でOS判定する
+            self.user_agent = navigator.userAgent.lower()
+            self.os_pc = not ("android" in self.user_agent or "iphone" in self.user_agent or "ipad" in self.user_agent)
+        else:
+            # ローカルから起動している場合、platformから判定する
+            self.os_name = platform.system()
+            self.os_pc =  self.os_name == "Windows" or self.os_name == "Darwin" or self.os_name == "Linux"
+
+        
+
+
         pyxel.init(width = 200, height = 355, title = "play M like SE")
-        self.init_se()
-        pyxel.mouse(True)
+
+        pyxel.mouse(self.os_pc)
+
+        self.init_sound()
+        
         pyxel.run(self.update, self.draw)
 
 
-    def init_se(self):
+    def init_sound(self):
         pyxel.sounds[0].set(
             "e4a3d3g2c2f1e4a3d3g2c2f1b0rrrrrrrrrrrrrrrrrrrrr" * 3,
             "S",
@@ -30,12 +53,17 @@ class M_like_SE():
             "7",
             "NNNNNF",
             15
-        )   # レベルアップ
+        )   # 1UP
 
     def update(self):
-        if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT) or pyxel.btnp(pyxel.KEY_SPACE):
+        if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
             x, y = pyxel.mouse_x, pyxel.mouse_y
-            pyxel.play(0,0)
+            if y <= 110:
+                pyxel.play(0,0)
+            elif 120 <= y and y <= 220:
+                pyxel.play(0,1)
+            elif 230 <= y:
+                pyxel.play(0,2)
             
         if pyxel.btnp(pyxel.KEY_D):
             pyxel.play(0,0)
@@ -43,17 +71,32 @@ class M_like_SE():
         if pyxel.btnp(pyxel.KEY_C):
             pyxel.play(0,1)
 
-
-
         if pyxel.btnp(pyxel.KEY_R):
             pyxel.play(0,2)
 
 
     def draw(self):
-        pyxel.cls(3)
-        pyxel.rect(x = 10,y = 10, w = 180, h = 100, col = 2)
-        pyxel.rect(x = 10, y = 120, w = 180, h = 100, col = 5)
-        pyxel.rect(x = 10, y = 230, w = 180, h = 100, col = 5)
+
+        writer = puf.Writer("misaki_gothic.ttf")
+
+        pyxel.cls(1)
+
+        pyxel.rect(x = 10,y = 10, w = 180, h = 100, col = 11)
+        writer.draw(x = 20, y = 20, text = "土管", font_size = 40, font_color = 2)
+        if self.os_pc:
+            writer.draw(x = 20, y = 70, text = "D", font_size = 30, font_color = 2)
+        
+        pyxel.rect(x = 10, y = 120, w = 180, h = 100, col = 10)
+        writer.draw(x = 20, y = 130, text = "コイン", font_size = 40, font_color = 1)
+        if self.os_pc:
+            writer.draw(x = 20, y = 180, text = "C", font_size = 30, font_color = 1)
+
+        pyxel.rect(x = 10, y = 230, w = 180, h = 100, col = 2)
+        writer.draw(x = 20, y = 240, text = "1UP", font_size = 40, font_color = 11)
+        if self.os_pc:
+            writer.draw(x = 20, y = 290, text = "R", font_size = 30, font_color = 11)
+
+
         
 
 if __name__ == "__main__":
