14f6e8808c8923eaac9aa1ab78042007414fdb87
Chikati0407 <130910609+Chikati0407@users.noreply.github.com>
2025-01-27 15:06:38 +0900

    音声設定の修正と新しい矩形描画の追加

--- game/game_se.py
+++ game/game_se.py
@@ -10,49 +10,50 @@ class M_like_SE():
 
 
     def init_se(self):
-        # pyxel.sounds[0].set(
-        #     "e4a3d3g2c2f1e4a3d3g2c2f1b0rrrrrrrrrrrrrrrrrrrrr" * 3,
-        #     "S",
-        #     "7",
-        #     "F",
-        #     1
-        # )   # 土管の音
         pyxel.sounds[0].set(
-            "b3e4e4e4e4e4e4e4",
+            "e4a3d3g2c2f1e4a3d3g2c2f1b0rrrrrrrrrrrrrrrrrrrrr" * 3,
             "S",
             "7",
-            "NNNNNNNF",
-            10
-        )   # コインの音
+            "F",
+            1
+        )   # 土管
         pyxel.sounds[1].set(
             "b3e4e4e4e4e4e4e4",
             "S",
             "7",
             "NNNNNNNF",
             10
-        )   # コインの音
+        )   # コイン
         pyxel.sounds[2].set(
             "e3g3e4c4d4g4",
             "S",
             "7",
             "NNNNNF",
             15
-        )
+        )   # レベルアップ
 
     def update(self):
         if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT) or pyxel.btnp(pyxel.KEY_SPACE):
+            x, y = pyxel.mouse_x, pyxel.mouse_y
             pyxel.play(0,0)
             
-        if pyxel.btnp(pyxel.KEY_Q):
+        if pyxel.btnp(pyxel.KEY_D):
+            pyxel.play(0,0)
+
+        if pyxel.btnp(pyxel.KEY_C):
             pyxel.play(0,1)
 
-        if pyxel.btnp(pyxel.KEY_W):
+
+
+        if pyxel.btnp(pyxel.KEY_R):
             pyxel.play(0,2)
 
 
     def draw(self):
         pyxel.cls(3)
-        pyxel.text(50, 180, "Press SPACE to play sound", pyxel.frame_count % 16)
+        pyxel.rect(x = 10,y = 10, w = 180, h = 100, col = 2)
+        pyxel.rect(x = 10, y = 120, w = 180, h = 100, col = 5)
+        pyxel.rect(x = 10, y = 230, w = 180, h = 100, col = 5)
         
 
 if __name__ == "__main__":
